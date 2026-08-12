34f6db7ef8ad952d06959ac8554f5b39f5b6a233
SlOVVliKE01 <thirakan.weef64@gmail.com>
2026-08-12 14:20:49 +0700

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,6 +1,6 @@
 // API Configuration
 // Deployed backend URL on Render
-const API_URL = 'https://solarad-2m7y.onrender.com';
+const API_URL = 'https://solaradsell.onrender.com';
 
 // For local development, use:
 // const API_URL = 'http://localhost:3000';
